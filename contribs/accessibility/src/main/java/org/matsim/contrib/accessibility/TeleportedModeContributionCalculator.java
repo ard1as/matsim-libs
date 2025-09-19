4efe9498239599aa0426b9fd90a3862f8ef84bd4
Ardias <tumiwa@campus.tu-berlin.de>
2025-09-19 16:23:28 +0200

    19.09.25 - added more attributes and their modifiers to scoring - unit tests that check relative modifiers - created interface for modifiers

--- contribs/accessibility/src/main/java/org/matsim/contrib/accessibility/TeleportedModeContributionCalculator.java
+++ contribs/accessibility/src/main/java/org/matsim/contrib/accessibility/TeleportedModeContributionCalculator.java
@@ -6,11 +6,9 @@ import org.matsim.api.core.v01.population.Activity;
 import org.matsim.api.core.v01.population.Leg;
 import org.matsim.api.core.v01.population.Person;
 import org.matsim.api.core.v01.population.PlanElement;
-import org.matsim.contrib.accessibility.accMods.accessibilityModifierInterface;
-import org.matsim.contrib.accessibility.accMods.economic_status;
+import org.matsim.contrib.accessibility.accMods.*;
 import org.matsim.contrib.accessibility.utils.AggregationObject;
 import org.matsim.core.config.groups.ScoringConfigGroup;
-import org.matsim.core.population.PersonUtils;
 import org.matsim.core.router.TripRouter;
 import org.matsim.facilities.*;
 
@@ -33,7 +31,9 @@ public class TeleportedModeContributionCalculator implements AccessibilityContri
 	private Map<Id<? extends BasicLocation>, AggregationObject> aggregatedOpportunities;
 	private ScoringConfigGroup scoringConfigGroup;
 
-	private final List<accessibilityModifierInterface> accMods = new ArrayList<>(); //todo for test accMods interface
+	//todo for test accMods interface (pre + post)
+	private final List<pre_accModsInterface> pre_accMods = new ArrayList<>();
+	private final List<post_accModsInterface> post_accMods = new ArrayList<>();
 
 	TeleportedModeContributionCalculator(String mode, TripRouter tripRouter, ScoringConfigGroup scoringConfigGroup) {
 
@@ -45,7 +45,10 @@ public class TeleportedModeContributionCalculator implements AccessibilityContri
 		this.betaDist_m = scoringConfigGroup.getModes().get(mode).getMarginalUtilityOfDistance();
 		this.asc = scoringConfigGroup.getModes().get(mode).getConstant();
 
-		accMods.add(new economic_status()); //todo register accMods here
+		//todo register accMods here
+		pre_accMods.add(new age());
+		post_accMods.add(new economic_status());
+		post_accMods.add(new sex());
 
 	}
 
@@ -110,24 +113,26 @@ public class TeleportedModeContributionCalculator implements AccessibilityContri
 			double teleportDist_m = walkLeg.getRoute().getDistance();
 			double departureTime_h = walkLeg.getDepartureTime().seconds() / 3600; //todo added departureTime_h
 
-			//todo penalty for old age (60+)
+			/* age modifiers (use interface instead)
+			//penalty for old age (60+)
 			if (PersonUtils.getAge(person) >= 60){
 				teleportTime_h *= 1.5; // walking time takes 1.5x as long for older person
 			}
-			/*if(PersonUtils.getAge(person) > 60) {
-				utilityTeleport -= 10.0;
-			}*/
-
-			//todo bonus for young age (15-29)
+//			if(PersonUtils.getAge(person) > 60) {
+//				utilityTeleport -= 10.0;
+//			}
+			//bonus for young age (15-29)
 			if (PersonUtils.getAge(person) >= 15 && PersonUtils.getAge(person) < 30){
 				teleportTime_h *= 0.9; // slightly faster walkers
+			}*/
+
+			//todo applying pre accMods using interface (age)
+			for (pre_accModsInterface mods : pre_accMods){
+				teleportTime_h = mods.apply(person, teleportTime_h, teleportDist_m, departureTime_h);
 			}
 
-			//todo penalty for restricted_mobility
 
-
-
-			//todo base utility
+			//todo base utility (for other modes, add betaMoney etc.)
 			double utilityTeleport = teleportTime_h * betaTT_h + teleportDist_m * betaDist_m + asc; // here can play around with weights
 			//	betaTT_h	= marginal utility of travel time (-)
 			//	betaDist_m	= marginal utility of distance (-)
@@ -142,30 +147,27 @@ public class TeleportedModeContributionCalculator implements AccessibilityContri
 			//todo per-opportunity accessibility score
 			double contribution = Math.exp(this.scoringConfigGroup.getBrainExpBeta() * utilityTeleport);
 
-			//todo test applying mods using interface*
-			for (accessibilityModifierInterface mods : accMods){
+			//todo applying post accMods using interface (economic_status, sex)
+			for (post_accModsInterface mods : post_accMods){
 				contribution = mods.apply(person, teleportTime_h, teleportDist_m, departureTime_h, contribution);
 			}
-			/*
-			//todo low economic_status penalty (scaling opportunity contributions)
+
+			/* low economic_status penalty (scaling opportunity contributions)
 			Object economic_status = person.getAttributes().getAttribute("economic_status");
 			if (economic_status != null && economic_status.equals("low")){
 				contribution *= 0.8;	// low income person has less access to/doesnt consider all available opportunities/facilities
-			}
-			*/
+			}*/
 
-
-			//todo departureTime penalty (very early/late less opportunity contributions)
+			/* departureTime penalty (very early/late less opportunity contributions)
 			if (departureTime_h < 6 || departureTime_h > 22){
 				contribution *=  0.5; // places are closed during these times
+			}*/
 
-			}
-
-			//todo departureTime penalty for women (perceived lower safety walking at  night)
+			/* departureTime penalty for women (perceived lower safety walking at  night)
 			Object sex = person.getAttributes().getAttribute("sex");
 			if (sex != null && sex.equals("f") && departureTime_h < 6 || departureTime_h > 22){
 				contribution *= 0.8;
-			}
+			}*/
 
 			//todo total accessibility including economic_status penalty
 			expSum += contribution;
